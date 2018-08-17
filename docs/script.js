11dd2d3f68c253acd1c7f68c85becd1540b9343e
Adrian <donayo71@gmail.com>
2018-08-17 18:02:36 +0200

    few changes

--- docs/script.js
+++ docs/script.js
@@ -1,27 +1,24 @@
 $(document).ready(function () {
-    $("#button1").click(function(){
-        let userInput = $("#usrInp").val().trim();
+    $(".search-button").click(function(){
+        let userInput = $(".search-input").val().trim();
         userInput = userInput.replace(/ /g, "+");
-        let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=ACSF6DvX2932HZzH0n7O6loDtrvWa543&limit=400";
+        let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=ACSF6DvX2932HZzH0n7O6loDtrvWa543&limit=100";
 
         $.ajax({ url: queryURL, method: 'GET' }).done(function (response) {
-
-            console.log(response.data);
 
             function getRandomInt(min, max) {
                 return Math.floor(Math.random() * (max - min + 1)) + min;
             }
 
-            for (let i=0;i<18;i++){
-                var giphyURL = response.data[getRandomInt(0, 400)].images.original.url;
+            for (let i=0;i<20;i++){
+                var giphyURL = response.data[getRandomInt(0, 100)].images.original.url;
                 var imgNr = "img"+i;
                 $('<img/>', {
                     'id': imgNr
                 }).appendTo("#window");
                 $("#"+imgNr).attr("src", giphyURL);
             }
-            console.log(giphyURL)
-
+            console.log($("#window").length);
 
         });
 
