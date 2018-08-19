da73cd7a9ba168248bd5ed4bea0771ed4c726c26
Unknown <anita.piechocka@interia.pl>
2018-08-19 20:42:59 +0200

    working passing value to input via hashtags

--- docs/script.js
+++ docs/script.js
@@ -111,15 +111,13 @@ $(document).ready(function () {
 
 });
 
-function getInput() {
-  userInput = $("#input")
-  userInput = $(".search-input").val().trim();
-  userInput = userInput.replace(/ /g, "+");
-  queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=ACSF6DvX2932HZzH0n7O6loDtrvWa543&limit=150";
-}
+// function getInput() {
+//   userInput = $("#input")
+//   userInput = $(".search-input").val().trim();
+//   userInput = userInput.replace(/ /g, "+");
+//   queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=ACSF6DvX2932HZzH0n7O6loDtrvWa543&limit=150";
+// }
 
-function jump(){
-  let search = document.getElementById("value");
-  let result = document.getElementById("input").value;
-  search.innerHTML = result;
+function jump(val){
+  document.getElementById('search-input-id').value=val;
 }
