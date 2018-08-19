d252ccceb39b44faef3e260e9c8e7c7246dd1de6
Jakub Kowalczyk <jkm.kowalczyk@gmail.com>
2018-08-19 18:26:40 +0200

    done some refactoring :)

--- docs/src/api.js
+++ docs/src/api.js
@@ -1,78 +1,59 @@
-  class Api {
-        /* ZMIENNE GLOBALNE */
-        let queryURL;
-        let randomURL;
-        let userInput;
-        let giphyURL;
-        let imgNr;
-        let resposneArguments;
-        let devChoice;
-        let numToGenerate;
-        /* WYWOŁANIE FUNKCJI WYŚWIETLAJĄCEJ NAJPOPULARNIEJSZE GIFY PO ZAŁADOWANIU STRONY */
-        randomGifs();
-        /* NAJPOPULARNIEJSZE GIFY */
-        function randomGifs() {
-            randomURL = "http://api.giphy.com/v1/gifs/trending?api_key=ACSF6DvX2932HZzH0n7O6loDtrvWa543";
-            $.ajax({ url: randomURL, method: 'GET' }).done(function (response) {
-                for (let i = 0; i < 20; i++) {
-                    giphyURL = response.data[i].images.original.url;
-                    imgNr = "img" + i;
+class Api {
+
+    constructor() {
+        document.querySelector('.search-button').addEventListener('click', () => this.getObject(20));
+    }
+
+
+    randomGifs = () => {
+        let randomURL = "http://api.giphy.com/v1/gifs/trending?api_key=ACSF6DvX2932HZzH0n7O6loDtrvWa543";
+        console.log("dsa");
+        $.ajax({url: randomURL, method: 'GET'}).done(function (response) {
+            for (let i = 0; i < 20; i++) {
+                let giphyURL = response.data[i].images.original.url;
+                let imgNr = "img" + i;
+                $("<img/>", {
+                    id: imgNr,
+                    class: "gifs"
+                }).appendTo("#content");
+                $("#" + imgNr).attr("src", giphyURL);
+            }
+        });
+    };
+
+
+    getObject(num) {
+        let userInput = $(".search-input").val().trim().replace(/ /g, "+");
+        let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=ACSF6DvX2932HZzH0n7O6loDtrvWa543&limit=150";
+
+        $.ajax({url: queryURL, method: 'GET'}).done(function (response) {
+            let resposneArguments = response.data.length;
+            let numToGenerate;
+
+            if (resposneArguments > num) {
+                numToGenerate = num;
+            } else {
+                numToGenerate = resposneArguments;
+            }
+
+            for (let i = 0; i < numToGenerate; i++) {
+                console.log(resposneArguments);
+                let giphyURL = response.data[i].images.original.url;
+                let imgNr = "img" + i;
+                if ($("#img" + (numToGenerate - 1)).length > 0) {
+                    $("#" + imgNr).attr("src", giphyURL);
+                }
+                else {
                     $("<img/>", {
                         id: imgNr,
                         class: "gifs"
                     }).appendTo("#content");
                     $("#" + imgNr).attr("src", giphyURL);
-
-                };
-            });
-        };
-        /* FUNKCJA ZWRACAJĄCA LOSOWĄ LICZBĘ */
-        function getRandomInt(min, max) {
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
-
-        /* FUNKCJA POBIERAJĄCA INPUT UŻYTKOWNIKA ORAZ GENERUJĄCA LINK DO API */
-        function getInput() {
-            userInput = $(".search-input").val().trim();
-            userInput = userInput.replace(/ /g, "+");
-            queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=ACSF6DvX2932HZzH0n7O6loDtrvWa543&limit=150";
-        }
-
-        /* FUNKCJA ZWRACAJĄCA OBJEKT DATA Z GIPHY API Z ARGUMENTEM ILE MA ICH WYGENEROWAĆ */
-        function getObject(num) {
-            devChoice = num;
-            $.ajax({ url: queryURL, method: 'GET' }).done(function (response) {
-                /* ZAWIERA LICZBĘ ARGUMENTÓW Z OBJEKTU DATA GIPHY API */
-                resposneArguments = response.data.length;
-                /* SPRAWDZA, CZY GIPHY ZWRACA TAKĄ SAMĄ LUB WIĘKSZĄ LICZBĘ ELEMENTÓW, JAKĄ WYBRAŁ UŻYTKOWNIK. JEŚLI NIE, WYŚWIETLI TYLE ILE ZWRÓCIŁ GIPHY */
-                if (resposneArguments > devChoice) {
-                    numToGenerate = devChoice;
                 }
-                else {
-                    numToGenerate = resposneArguments;
-                };
-                /* GENERUJE NOWE GIFY POCZĄWSZY OD POZYCJI 0 KOŃCZĄC NA POZYCJI W FUNKCJI howMany */
-                for (let i = 0; i < numToGenerate; i++) {
-                    giphyURL = response.data[i].images.original.url;
-                    console.log(resposneArguments);
-                    var imgNr = "img" + i;
-                    if ($("#img" + (numToGenerate - 1)).length > 0) {
-                        $("#" + imgNr).attr("src", giphyURL);
-                    }
-                    else {
-                        $("<img/>", {
-                            id: imgNr,
-                            class: "gifs"
-                        }).appendTo("#content");
-                        $("#" + imgNr).attr("src", giphyURL);
-                    };
-                };
-            });
-        };
-
-        $(".search-button").click(function () {
-            getInput();
-            getObject(20);
+            }
         });
-    }
+    };
+
+}
+
 export default Api;
