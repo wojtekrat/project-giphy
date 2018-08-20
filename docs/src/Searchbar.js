a4c9aa9a3133b6a74a44f2d306f82ea8571784e6
Jakub Kowalczyk <jkm.kowalczyk@gmail.com>
2018-08-20 18:58:48 +0200

    all working well, ready to be merged to master

--- docs/src/Searchbar.js
+++ docs/src/Searchbar.js
@@ -1,20 +1,20 @@
 class Searchbar {
 
 
-    searchbarPlaceholder = $('.search-input').placeholder;
+    searchbar = $('.search-input');
 
     constructor() {
         this.flag = false;
-        this.searchbarPlaceholder = 'Search various GIFs and Stickers!';
+        this.searchbar.attr('placeholder', 'Search various GIFs and Stickers!');
         setInterval(this.changePlaceholder, 2000);
     }
 
     changePlaceholder = () => {
         if (this.flag) {
-            this.searchbarPlaceholder = 'Search various GIFs and Stickers!';
+            this.searchbar.attr('placeholder', 'Search various GIFs and Stickers!');
             this.flag = !this.flag;
         } else {
-            this.searchbarPlaceholder = '@username + tag to search within a verified channel';
+            this.searchbar.attr('placeholder', '@username + tag to search within a verified channel');
             this.flag = !this.flag;
         }
     };
