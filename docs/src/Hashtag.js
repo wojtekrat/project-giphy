895e84effa13b896dcd21171b17b4bea3ee2b87f
Jakub Kowalczyk <jkm.kowalczyk@gmail.com>
2018-08-20 21:20:53 +0200

    some more fixes...

--- docs/src/Hashtag.js
+++ docs/src/Hashtag.js
@@ -1,8 +1,14 @@
 class Hashtag {
+
     constructor() {
-        let jump = (val) => {
-            document.getElementById('search-input-id').value = val;
-        }
+        document.getElementById('hashtag1').addEventListener('click', () => this.search("wheel"));
+        document.getElementById('hashtag2').addEventListener('click', () => this.search("paola"));
+        document.getElementById('hashtag3').addEventListener('click', () => this.search("raz degan"));
+        document.getElementById('hashtag4').addEventListener('click', () => this.search("grand theft auto"));
+    }
+
+    search = (val) => {
+        document.getElementById('search-input').value = val;
     }
 }
 
