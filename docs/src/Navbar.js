61e428f131531bf220ef1ef9bef8ec41de812d06
Jakub Kowalczyk <jkm.kowalczyk@gmail.com>
2018-08-21 12:40:13 +0200

    fixed some responsive issues

--- docs/src/Navbar.js
+++ docs/src/Navbar.js
@@ -1,23 +1,35 @@
 class Navbar {
 
-    navbarPosition = $("#navbar").offset().top;
+    navbarPosition = $("#searchbar").offset().top;
 
     constructor() {
         window.addEventListener('scroll', this.sticky);
+        window.addEventListener('resize', this.sticky);
     }
 
     sticky = () => {
         if ($(window).scrollTop() > this.navbarPosition) {
             $("#navbar").addClass("sticky");
-            $('#search-input').css({
-                'margin-left': "240px"
-            });
+            if ($(window).width() > 950) {
+                $('#search-input').css({
+                    'margin-left': "240px",
+                });
+                $('#logo').css({
+                    'opacity': '1'
+                });
+            } else {
+                $('#search-input').css({
+                    'margin-left': "0px",
+                });
+                $('#logo').css({
+                    'opacity': '0'
+                });
+            }
             $('#logo').css({
                 'position': 'fixed',
                 'padding': '5px 15px',
                 'align-items': 'center'
-            })
-            ;
+            }).addClass("position-fixed");
             $("#hashtags-container").slideUp();
         } else {
             $("#navbar").removeClass("sticky");
@@ -27,8 +39,9 @@ class Navbar {
             $('#logo').css({
                 'position': 'relative',
                 'padding': '10px 0',
-                'align-items': 'initial'
-            });
+                'align-items': 'initial',
+                'opacity': '1'
+            }).removeClass("position-fixed");
             $("#hashtags-container").delay(50).slideDown();
         }
     };
